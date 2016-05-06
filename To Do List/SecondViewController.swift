c4478016176369bf01c295dee2835dbf98846569
adityamaru27 <a2maru@uwaterloo.ca>
2016-05-06 11:49:26 -0400

    ToDo

--- To Do List/SecondViewController.swift
+++ To Do List/SecondViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var textInput: UITextField!
+    
+    
+    @IBAction func submitItem(sender: AnyObject)
+    {
+        toDoList.append(textInput.text!)
+        textInput.text = ""
+        NSUserDefaults.standardUserDefaults().setObject(toDoList, forKey: "toDoList")
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +31,17 @@ class SecondViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?)
+    {
+        self.view.endEditing(true)
+        
+    }
+    
+    func textFieldShouldReturn(textField : UITextField!) -> Bool
+    {
+        textInput.resignFirstResponder()
+        return true
+    }
 }
 
